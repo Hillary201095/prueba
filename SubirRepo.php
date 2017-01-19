dd0319645993dcabffcdd08847883cfd952ded15
Hillary201095 <xd_echelona_xd@hotmail.com>
2017-01-19 11:18:17 -0600

    ComandosBasicos

---
+++ SubirRepo.php
@@ -0,0 +1,15 @@
+git init
+touch README.md
+vim README.md
+git add README.md
+git config --global user.name 'Manuel9507'
+git config --global user.email 'manuale_flores07@hotmail.com'
+git commit -m "lo que sea"
+git remote add origin git@github.com:Manuel9507/Hola.git
+git push -u origin master
+
+#crear nuevo archivo
+touch alo.php
+vim alo.php
+git add alo.php
+git commit -m "prueba"
